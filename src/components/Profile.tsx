01a3c8780b2414a3ccd213c90f009ca17e0f49c2
Pedro <pedro.nunes@southsystem.com.br>
2021-02-27 07:18:21 -0300

    created profile Component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/pedrofnuness.png" alt="Pedro Nunes" />
+      <div>
+        <strong>Pedro Nunes</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
